6fa45df7b0fdbe62f413c5445b19b616b2fbbccf
Gritzaev Nikita <gritzaevnikita@gmail.com>
2024-02-22 22:30:50 +0700

    Refactor

--- frontend/web-app-client/src/pages/main/ui/mainPage.tsx
+++ frontend/web-app-client/src/pages/main/ui/mainPage.tsx
@@ -6,7 +6,7 @@ import { MainMenuItems } from 'pages/main/config'
 export const MainPage = () => {
   return (
     <Center>
-      <h1>Добро пожаловать</h1>
+      <h1 className='text-center'>Добро пожаловать</h1>
       <h2 className='my-3'>Выберите пункт меню</h2>
       <Flex wrap={'wrap'} justify='center' align='center' className='mb-6'>
         {MainMenuItems.map((item, index) => (
