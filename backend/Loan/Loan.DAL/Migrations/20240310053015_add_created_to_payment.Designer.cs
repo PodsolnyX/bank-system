0b6062e7821b8f2bb28cd5812a6b36f9858201b2
Terekhov Ivan <shorop5@mail.ru>
2024-03-10 12:31:01 +0700

    fix

--- backend/Loan/Loan.DAL/Migrations/20240310053015_add_created_to_payment.Designer.cs
+++ backend/Loan/Loan.DAL/Migrations/20240310053015_add_created_to_payment.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Loan.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Loan.DAL.Migrations
 {
     [DbContext(typeof(LoanDbContext))]
-    partial class LoanDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240310053015_add_created_to_payment")]
+    partial class add_created_to_payment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Loan/Loan.DAL/Migrations/20240310053015_add_created_to_payment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Loan.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_created_to_payment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Payments",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Payments",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Payments",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Payments");
+        }
+    }
+}
