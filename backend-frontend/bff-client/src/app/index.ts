0152ff479a25d4520d15653c48d36da2e0fa1e24
Gritzaev Nikita <gritzaevnikita@gmail.com>
2024-05-01 17:24:58 +0700

    fix

--- backend-frontend/bff-client/src/app/index.ts
+++ backend-frontend/bff-client/src/app/index.ts
@@ -15,8 +15,8 @@ for (const stage of pipeline) {
 
 app.listen(PORT, () => console.log(`Express - localhost:${PORT}`))
 
-process.on('uncaughtException', (err) => {
-  console.log(`Error`, err)
+process.on('uncaughtException', () => {
+  //console.log(`Error`, err)
 })
 
 const wss = new WebSocketServer({ port: WS_PORT, path: WS_PATH })
--- backend-frontend/bff-client/src/middleware/BeforeReq.ts
+++ backend-frontend/bff-client/src/middleware/BeforeReq.ts
@@ -3,18 +3,22 @@ import { NextFunction, Request, Response } from 'express'
 import { CB, mapBffRoute } from 'middleware/CircuitBreaker'
 import { ObserverService } from 'services/ObserverService'
 
+const CB_ACTIVE = false
+
 export const BeforeReqFn =
-  (ObserverService: ObserverService,) =>
+  (ObserverService: ObserverService) =>
   async (req: Request, res: Response, next: NextFunction) => {
     req.headers[REQUEST_ID_HEADER] = Math.random().toString(16).slice(2)
     req.headers[REQUEST_TIME_START_HEADER] = Date.now().toString()
 
-    const mapping = mapBffRoute(req.url)
-    if (mapping) {
-      if (!CB.Pass(mapping)) {
-        res.status(503).send('Circuit Breaker')
-        ObserverService.Collect(req, 503, 'Circuit Breaker')
-        return
+    if (CB_ACTIVE) {
+      const mapping = mapBffRoute(req.url)
+      if (mapping) {
+        if (!CB.Pass(mapping)) {
+          res.status(503).send('Circuit Breaker')
+          ObserverService.Collect(req, 503, 'Circuit Breaker')
+          return
+        }
       }
     }
 
