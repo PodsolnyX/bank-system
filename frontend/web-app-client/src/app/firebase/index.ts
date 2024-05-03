eb2b0747bf23a1ed8c6cc389de7dcc7c826d2279
Gritzaev Nikita <gritzaevnikita@gmail.com>
2024-05-03 21:02:43 +0700

    test firebase

---
+++ frontend/web-app-client/src/app/firebase/index.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { getMessaging } from "firebase/messaging";
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYk04pAROpajVpve3-7Snx4VhxjXshnFU",
+  authDomain: "test-27676.firebaseapp.com",
+  projectId: "test-27676",
+  storageBucket: "test-27676.appspot.com",
+  messagingSenderId: "512336316252",
+  appId: "1:512336316252:web:44dbd234256c8ea21a6399",
+  measurementId: "G-KFPC51MKQB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const messaging = getMessaging(app);
--- frontend/web-app-client/src/app/index.tsx
+++ frontend/web-app-client/src/app/index.tsx
@@ -1,5 +1,6 @@
+import { getToken } from "firebase/messaging";
 import { useAuth } from 'oidc-react'
-
+import { useEffect } from "react";
 import { BanPage } from 'pages/ban'
 import { ErrorPage } from 'pages/error'
 import { useTheme } from 'features/preferences'
@@ -7,9 +8,25 @@ import { useGetHiddenAccountsQuery } from 'entities/preferences'
 import { useGetAccessInfoQuery } from 'entities/user'
 import { Spinner } from 'shared/ui'
 
+import { messaging } from "./firebase";
 import { StoreProvider, Toaster, AppAuthProvider, AntdProvider } from './providers'
 import { ApplicationRouter } from './router'
 import './styles/index.scss'
+
+
+async function requestPermission() {
+  //requesting permission using Notification API
+  const permission = await Notification.requestPermission();
+
+  if (permission === "granted") {
+    const token = await getToken(messaging, {
+      vapidKey: 'BF5uUatJxEY07grBcr9rI4rNISVtrioC9O_PBgR_YWTMiMZjggaMwGfjA2Hl-pnJsV1M9CiAHj_PwUb_CWFyyNs',
+    });
+    console.log("Token generated : ", token);
+  } else if (permission === "denied") {
+    alert("You denied for the notification");
+  }
+}
 
 function App() {
   useTheme()
@@ -25,6 +42,10 @@ function App() {
   const isLoading = isAuthLoading || hiddenAccs.isFetching || accessQuery.isFetching
   const isError = hiddenAccs.isError || accessQuery.isError
   const isBanned = accessQuery.data?.bannedAt !== null
+
+  useEffect(() => {
+    requestPermission()
+  },[])
   return isLoading ? (
     <Spinner />
   ) : isError ? (
