98fe23a84288b913b50b6401302fdd3f2a42e22e
Gritzaev Nikita <gritzaevnikita@gmail.com>
2024-05-03 22:18:37 +0700

    fix messages repeat

--- frontend/web-app-client/public/firebase-messaging-sw.js
+++ frontend/web-app-client/public/firebase-messaging-sw.js
@@ -1,13 +1,10 @@
 /* eslint-disable no-undef */
-
-// Scripts for firebase and firebase messaging
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts(
   "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
 );
 
-// Initialize the Firebase app in the service worker
-// "Default" Firebase configuration (prevents errors)
+
 const defaultConfig = {
   apiKey: true,
   projectId: true,
@@ -20,12 +17,12 @@ firebase.initializeApp(defaultConfig);
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title;
+messaging.onBackgroundMessage(() => {
+/*const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
     icon: payload.notification.image,
   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);*/
 });
