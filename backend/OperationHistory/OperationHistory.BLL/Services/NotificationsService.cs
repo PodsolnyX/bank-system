8a622178325adf5fe2b36dc4795a3e686f5a78e4
Terekhov Ivan <shorop5@mail.ru>
2024-05-04 13:45:31 +0700

    fix

--- backend/OperationHistory/OperationHistory.BLL/Services/NotificationsService.cs
+++ backend/OperationHistory/OperationHistory.BLL/Services/NotificationsService.cs
@@ -22,14 +22,14 @@ public class NotificationsService {
 
     public async Task SendFirebase(OperationHistoryMessage messageDto) {
         FirebaseApp.Create(new AppOptions() {
-            Credential = GoogleCredential.FromFile("../../OperationHistory/OperationHistory.BLL/test-27676-firebase-adminsdk-xyxen-11d662c6eb.json")
+            Credential = GoogleCredential.FromFile("../OperationHistory.BLL/test-27676-firebase-adminsdk-xyxen-11d662c6eb.json")
         });
         var messaging = FirebaseMessaging.DefaultInstance;
         var result = await messaging.SendEachAsync([new Message {
             Notification = new Notification {
                 Title = messageDto.OperationType + " " + messageDto.DateTime,
                 Body = messageDto.Amount + " " + messageDto.CurrencyType,
-                ImageUrl = "https://telegra.ph/file/1f2b6e7fadf56fb39c618.jpg"
+                ImageUrl = "https://ihealth.in.ua/wp-content/uploads/2021/09/shutterstock_923986961.jpg"
             },
             Token =
                 "epGz-IrsDm9PdLzduCTbKJ:APA91bH_j_k82pi_QwLkYSR4fXjhrwrCsf6y6MaS10xfW4XxX1tHphHiE2jS7g2P8waisS3AjSE4VHNS1D6W1lDE4Bn8NdBaqW5RQ1MTwRgY69YkOnYISwIi51KLyWAhUaIWY1fKCZpb",
@@ -39,7 +39,7 @@ public class NotificationsService {
             Notification = new Notification {
                 Title = messageDto.OperationType + " " + messageDto.DateTime,
                 Body = messageDto.Amount + " " + messageDto.CurrencyType,
-                ImageUrl = "https://telegra.ph/file/1f2b6e7fadf56fb39c618.jpg"
+                ImageUrl = "https://ihealth.in.ua/wp-content/uploads/2021/09/shutterstock_923986961.jpg"
             },
             Token =
                 "elR57jFj0qu6kay6lhG9Ww:APA91bHxzJQfGLhqTEfYkeXQDGgK3yfB4WqKMam8oZN2BHeBeK6gcQeLuY2OOLr4NS8HbcVOiZWXGJ2ZDYFrU8rJiFAhwa7hcz2YrSWwCcjtW1OSdIE0D8-d36eyBREWvHi7qgOYsOEF",
